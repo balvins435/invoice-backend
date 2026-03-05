5ee3268e27f1f32fbea7781a4f80e4f12492cddb
balvins435 <vincentkiplangat02@gmail.com>
2026-03-05 19:24:05 +0300

    Created new apps; messaging,payments

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Payment(models.Model):
+    business=models.ForeignKey('Business', on_delete=models.CASCADE)
+    invoice=models.ForeignKey('Invoice', on_delete=models.CASCADE)
+
+    amount=models.DecimalField(max_digits=10, decimal_places=2)
