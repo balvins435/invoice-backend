440d71bc203426fd68bae7947a740ae1875b7007
balvins435 <vincentkiplangat02@gmail.com>
2026-03-31 15:00:14 +0300

    modified views and urls

--- ai/views.py
+++ ai/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .services.invoice_ai import generate_invoice_from_text
+
+@api_view(['POST'])
+def generate_invoice(request):
+    text = request.data.get("text")
+
+    if not text:
+        return Response({"error": "Text is required"}, status=400)
+
+    data = generate_invoice_from_text(text)
+    return Response(data)
